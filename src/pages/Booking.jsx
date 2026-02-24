684e988598f1658a4295eddeb36d0f9fe3c91ea1
jeedb <jeedb7810@edu.sunne.se>
2026-02-24 14:41:12 +0100

    Create Booking page with BookingForm component

---
+++ src/pages/Booking.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import BookingForm from '../components/BookingForm'
+
+function Booking() {
+
+
+  return (
+    <div>
+        <h1>BOOKING</h1>
+        <BookingForm />
+    </div>
+  )
+}
+
+export default Booking
